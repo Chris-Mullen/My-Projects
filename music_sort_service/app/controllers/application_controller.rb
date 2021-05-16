7fbd1516aee389b9922465c7c0252053e0f71fef
CM066014 <cm066014@cerner.net>
2021-05-16 03:52:21 +0100

    Adds method for rendering errors in json format

--- music_sort_service/app/controllers/application_controller.rb
+++ music_sort_service/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  # Public: Display error messages in JSON format
+    def render_json_validation_error(resource)
+      render  status: :bad_request,
+              adapter: :json_api,
+              serializer: ActiveModel::Serializer::ErrorSerializer,
+              json: resource
+  end
 end
