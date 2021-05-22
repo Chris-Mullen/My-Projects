c206d1d9aac6a5661393cfe0e60c816053543c0e
CM066014 <cm066014@cerner.net>
2021-05-22 16:53:48 +0100

    Adds artist post functionality; Updates album attributes to match recent changes

--- music_sort_service/app/controllers/music_sort_service/v1/artists_controller.rb
+++ music_sort_service/app/controllers/music_sort_service/v1/artists_controller.rb
@@ -4,7 +4,23 @@ module MusicSortService
   module V1
     # Controller for managing artists
     class ArtistsController < ApplicationController
+      def create
+        artist = Artist.new(artist_params)
+        unless artist.save
+          render_json_validation_error artist
+          return
+        end
+
+        render json: artist, status: :created
+      end
+
       def show; end
+
+      private
+
+      def artist_params
+        params.require(:artist).permit(:title)
+      end
     end
   end
 end
--- music_sort_service/db/migrate/20210516012641_create_albums.rb
+++ music_sort_service/db/migrate/20210516012641_create_albums.rb
@@ -6,10 +6,9 @@ class CreateAlbums < ActiveRecord::Migration[6.1]
     create_table :albums, id: false do |t|
       t.string :uuid, limit: 36, primary: true, null: false
       t.string :title
-      t.string :artist_id, null: false, foreign_key: true, index: true
       t.integer :year
-      t.integer :total_tracks
-      t.integer :total_disks, default: 1
+      t.integer :tracks
+      t.integer :disks, default: 1
 
       t.timestamps
     end
